81dcf7a294b8e4d570a6024d86451b4336b80e09
Mark Ekisa <mark.ekisa@gmail.com>
2021-01-18 14:29:44 +0300

    Bump io.aviso/pretty version to 0.1.37
    Signed-off-by: Mark Ekisa <mark.ekisa@gmail.com>

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
-(defproject onelog "0.5.1"
+(defproject ivermac/onelog "0.5.2"
   :description "Batteries-included logging for Clojure"
-  :url "https://github.com.com/pjlegato/onelog"
+  :url "https://github.com.com/ivermac/onelog"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [
@@ -8,6 +8,6 @@
                  [clj-logging-config "1.9.10" :exclusions [log4j]]
                  [org.clojars.pjlegato/clansi "1.3.0"]
                  [org.slf4j/slf4j-log4j12 "1.7.7"]
-                 [io.aviso/pretty "0.1.30"] ;; Pretty printer / exception formatter
+                 [io.aviso/pretty "0.1.37" :exclusions [org.clojure/clojure]] ;; Pretty printer / exception formatter
                  ])
 
